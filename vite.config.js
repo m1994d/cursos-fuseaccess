ff5350a2e3e17e9dfbc968c889362aa2dfb07ba6
m1994d <marlondiaz2994@gmail.com>
2025-09-09 23:50:51 -0500

    Curso

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cursos-fuseaccess/', // Cambia esto por el nombre de tu repositorio
+  base: '/cursos-fuseaccess/', // Debe coincidir exactamente con el nombre de tu repo
 })
