0d5ccafce9ac7a9e067d6f12352e2d56b099b82c
m1994d <marlondiaz2994@gmail.com>
2025-09-11 09:05:33 -0500

    Fix vercel and git-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cursos-fuseaccess/'
+  base: process.env.VERCEL ? '/' : '/cursos-fuseaccess/'
 })
