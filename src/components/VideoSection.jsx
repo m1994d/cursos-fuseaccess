82c686b17da2536d45bb9f5eda6747415e9dbf6d
m1994d <marlondiaz2994@gmail.com>
2025-09-17 21:14:46 -0500

    list

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -53,7 +53,7 @@ export default function VideoSection({ category }) {
     fundamentos: [
       {
         title: 'Fundamentos electricidad',
-        url: 'https://www.youtube.com/embed/playlist?list=PL5u_oyI4fV4TQccaqP9IVoyPZFzDQ-0zw'
+        url: 'https://www.youtube.com/embed/8bjuNlIhZXs&list=PLb7PdgLBShiqyp1lEJ2IPYukFnATttAG0'
       },
       { title: 'Curso tactico de tuberia eléctrica', 
         url: 'https://www.youtube.com/c/PL5u_oyI4fV4TQccaqP9IVoyPZFzDQ-0zw'
