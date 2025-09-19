8437f1ca8310424154eae8d8c3472d4745eb4139
m1994d <marlondiaz2994@gmail.com>
2025-09-18 20:31:54 -0500

    ok ok

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -1,63 +1,84 @@
 export default function VideoSection({ category }) {
   const videos = {
-    cctv: [
-      { title: 'Curso CCTV Básico (analogica y IP) 1/3 ', url: 'https://www.youtube.com/embed/CeK1ax_g3Rw' },
-      { title: 'Curso CCTV Básico (analogica y IP) 2/3', url: 'https://www.youtube.com/embed/ZLvOOHGhxiY' },
-      { title: 'Curso CCTV Básico (analogica y IP) 3/3', url: 'https://www.youtube.com/embed/bMN50W6ov8s' }
-    ],
-    aplicaciones: [
-      { title: 'Aplicación FuseAccess', url: 'https://www.youtube.com/embed/QvEoLIN1DA4' },
-      { title: 'Apicación HikConnect', url: 'https://www.youtube.com/embed/aJou53c80ws' },
-      { title: 'Aplicación HikPartner Pro', url: 'https://www.youtube.com/embed/mcaLvBqrtxY' }
-    ],
-    acceso: [
-      { title: 'Control de Acceso y Asistencia Hikvision', url: 'https://www.youtube.com/embed/UkvivvTlzDs' },
-      { title: 'Biometría y Seguridad', url: 'https://www.youtube.com/embed/_Qvd4f1Xt30' },
-      { title: 'Alarma de intrusión inalámbrica AX HOME', url: 'https://www.youtube.com/embed/gycAAjGEiwY' },
-      { title: 'Como Configurar Alarma HIkvision AXPRO', url: 'https://www.youtube.com/embed/L2Z76_sDJxM' },
-      { title: 'Nueva terminal facial MinMoe K1T344 de HikVision', url: 'https://www.youtube.com/embed/bCoLZXrPB7Q' }
-    ],
-    manuales: [
-      { title: 'Cómo estudiar y tomar apuntes', url: 'https://www.youtube.com/embed/xQr-HWyA2Oo' },
-    ],
-    fundamentos: [
-      { title: 'Curso tactico de tubería EMT', url: 'https://www.youtube.com/embed/8bjuNlIhZXs&list=PLb7PdgLBShiqyp1lEJ2IPYukFnATttAG0' },
-      { title: ' Cómo hacer una instalación ELÉCTRICA segura', url: 'https://www.youtube.com/embed/gHBO3zYcRZI' },
-      { title: 'Instalación eléctrica paso a paso', url: 'https://www.youtube.com/embed/ELqsMCCNlvA' }
-    ]
+    cctv: {
+      equipos: [
+        { title: 'Curso CCTV Básico (analogica y IP) 1/3 ', url: 'https://www.youtube.com/embed/CeK1ax_g3Rw' },
+        { title: 'Curso CCTV Básico (analogica y IP) 2/3', url: 'https://www.youtube.com/embed/ZLvOOHGhxiY' },
+        { title: 'Curso CCTV Básico (analogica y IP) 3/3', url: 'https://www.youtube.com/embed/bMN50W6ov8s' }
+      ]
+    },
+    aplicaciones: {
+      apps: [
+        { title: 'Aplicación FuseAccess', url: 'https://www.youtube.com/embed/QvEoLIN1DA4' },
+        { title: 'Apicación HikConnect', url: 'https://www.youtube.com/embed/aJou53c80ws' },
+        { title: 'Aplicación HikPartner Pro', url: 'https://www.youtube.com/embed/mcaLvBqrtxY' }
+      ]
+    },
+    acceso: {
+      biometria: [
+        { title: 'Control de Acceso y Asistencia Hikvision', url: 'https://www.youtube.com/embed/UkvivvTlzDs' },
+        { title: 'Biometría y Seguridad', url: 'https://www.youtube.com/embed/_Qvd4f1Xt30' }
+      ],
+      alarmas: [
+        { title: 'Alarma de intrusión inalámbrica AX HOME', url: 'https://www.youtube.com/embed/gycAAjGEiwY' },
+        { title: 'Como Configurar Alarma HIkvision AXPRO', url: 'https://www.youtube.com/embed/L2Z76_sDJxM' }
+      ],
+      facial: [
+        { title: 'Nueva terminal facial MinMoe K1T344 de HikVision', url: 'https://www.youtube.com/embed/bCoLZXrPB7Q' }
+      ]
+    },
+    manuales: {
+      apuntes: [
+        { title: 'Cómo estudiar y tomar apuntes', url: 'https://www.youtube.com/embed/xQr-HWyA2Oo' }
+      ]
+    },
+    fundamentos: {
+      electricidad: [
+        { title: 'Curso tactico de tubería EMT', url: 'https://www.youtube.com/embed/8bjuNlIhZXs&list=PLb7PdgLBShiqyp1lEJ2IPYukFnATttAG0' },
+        { title: 'Cómo hacer una instalación ELÉCTRICA segura', url: 'https://www.youtube.com/embed/gHBO3zYcRZI' },
+        { title: 'Instalación eléctrica paso a paso', url: 'https://www.youtube.com/embed/ELqsMCCNlvA' }
+      ]
+    }
   };
 
-  // Mapea los nombres de las categorías a subtítulos legibles
-  const categoryTitles = {
-    cctv: 'Videos de CCTV',
-    aplicaciones: 'Videos de Aplicaciones',
-    acceso: 'Videos de Control de Acceso',
-    manuales: 'Videos de Manuales',
-    fundamentos: 'Videos de Fundamentos'
+  // Nombres legibles para las materias
+  const materiaTitles = {
+    equipos: 'Equipos',
+    apps: 'Aplicaciones',
+    biometria: 'Biometría',
+    alarmas: 'Alarmas',
+    facial: 'Reconocimiento Facial',
+    apuntes: 'Apuntes',
+    electricidad: 'Electricidad'
   };
 
-  const categoryVideos = videos[category] || [];
-  const subtitle = categoryTitles[category] || 'Videos';
+  const categoryMaterias = videos[category] || {};
 
   return (
     <section className="video-section">
       <h3>Videos</h3>
-      <h4 style={{ marginTop: '1rem', marginBottom: '1rem', color: '#1B81FF' }}>{subtitle}</h4>
-      <div className="video-grid">
-        {categoryVideos.map((video, index) => (
-          <div key={index} className="video-item">
-            <h5>{video.title}</h5>
-            <iframe
-              width="300"
-              height="200"
-              src={video.url}
-              title={video.title}
-              frameBorder="0"
-              allowFullScreen
-            ></iframe>
+      {Object.entries(categoryMaterias).map(([materia, vids]) => (
+        <div key={materia}>
+          <h4 style={{ margin: '1.5rem 0 1rem 0', color: '#1B81FF' }}>
+            {materiaTitles[materia] || materia}
+          </h4>
+          <div className="video-grid">
+            {vids.map((video, index) => (
+              <div key={index} className="video-item">
+                <h5>{video.title}</h5>
+                <iframe
+                  width="300"
+                  height="200"
+                  src={video.url}
+                  title={video.title}
+                  frameBorder="0"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </section>
   );
 }
